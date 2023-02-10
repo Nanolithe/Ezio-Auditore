412fe233f8bab2d328eff6143ace91040b79aa1b
Nanolithe <39149226+Nanolithe@users.noreply.github.com>
2023-02-09 23:37:43 -0800

    Add files via upload

--- Ezio-Auditore2/Ezio-Auditore2/ViewController.swift
+++ Ezio-Auditore2/Ezio-Auditore2/ViewController.swift
@@ -14,6 +14,30 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    
+    @IBAction func didTapButton(_ sender: UITapGestureRecognizer) {
+        if let tappedView = sender.view {
+            performSegue(withIdentifier: "detailSegue", sender: tappedView)
+        }
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
 
+        if segue.identifier == "detailSegue",
+           let tappedView = sender as? UIView,
+        let detailViewController = segue.destination as? DetailViewController {
+
+            if tappedView.tag == 0 {
+                print("Hi there")
+            } else if tappedView.tag == 1 {
+                print("Howdy")
+            } else if tappedView.tag == 2 {
+                print("Hello")
+            } else {
+                print("nothing.")
+            }
+        }
+    }
+    
 }
 
